dd38c2e16e3bd2c8e21ce665610443da2c2a752a
Jill Anderson <54604783+jianderson@users.noreply.github.com>
2021-09-27 13:28:00 -0700

    add Unary minus to the funtions of the Calculator, also a start to the divide funtion but not working yet

--- Assignment1/Part2/Interpreter.hs
+++ Assignment1/Part2/Interpreter.hs
@@ -7,3 +7,6 @@ eval (Num n) = n
 eval (Plus n m) = (eval n) + (eval m)
 eval (Times n m) = (eval n) * (eval m)
 eval (Minus n m) = (eval n) - (eval m)
+--eval (Divide n m) = (eval n) / (eval m)
+--eval (Mod n m) = (eval n) 'mod' (eval m)
+eval (Neg n) = - (eval n)
