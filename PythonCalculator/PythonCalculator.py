2bc98ca37298786330b2a6cf6719306223d5b9c2
Jill Anderson <54604783+jianderson@users.noreply.github.com>
2021-10-15 11:58:43 -0700

    multiplication does not work

--- PythonCalculator/PythonCalculator.py
+++ PythonCalculator/PythonCalculator.py
@@ -31,25 +31,35 @@ def abs(first):
 
 
 
+def main():
 
+    sys.argv[1] = int(sys.argv[1])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
+    print(sys.argv[0])
+    sys.argv[3] = int(sys.argv[3])
 
-if sys.argv[1] == "*":
-    print(multiply(sys.argv[0], sys.argv[2]))
+    if sys.argv[2] == "+":
+        print(add(sys.argv[1], sys.argv[3]))
 
-if sys.argv[1] == "+":
-    print(add(sys.argv[0], sys.argv[2]))
+    if sys.argv[2] == "-":
+        print(minus(sys.argv[1], sys.argv[3]))
 
-if sys.argv[1] == "-":
-    print(minus(sys.argv[0], sys.argv[2]))
+    if ("*") in sys.argv:
+        print(multiply(sys.argv[1], sys.argv[3]))
 
-if sys.argv[1] == "/":
-    print(divide(sys.argv[0], sys.argv[2]))
+    if sys.argv[2] == "/":
+        print(divide(sys.argv[1], sys.argv[3]))
 
-if sys.argv[1] == "%":
-    print(mod(sys.argv[0], sys.argv[2]))
+    if sys.argv[2] == "%":
+        print(mod(sys.argv[1], sys.argv[3]))
 
-if sys.argv[1] == "^":
-    print(power(sys.argv[0], sys.argv[2]))
+    if sys.argv[2] == "^":
+        print(power(sys.argv[1], sys.argv[3]))
 
-if sys.argv[0] == "|":
-    print(abs(sys.argv[1]))
+    if sys.argv[1] == "|":
+        print(abs(sys.argv[2]))
+
+if __name__ == "__main__":
+    main()
