1b50f024308222dff647d853fd1fd7d3db76a86e
Jill Anderson <54604783+jianderson@users.noreply.github.com>
2021-10-15 11:31:05 -0700

    adds very basic python calculator

---
+++ PythonCalculator/PythonCalculator.py
@@ -0,0 +1,36 @@
+
+
+#user input: 5 * 6
+# argv[1]
+import sys
+import math
+
+
+
+def multiply(first, second):
+    return first * second
+
+def add(first, second):
+    return first + second
+
+def minus(first, second):
+    return first - second
+
+def divide(first, second):
+    return first/second
+
+def mod(first, second):
+    return first % second
+
+def power(first, second):
+    return pow(first, second)
+
+def abs(first):
+    return abs(first)
+
+
+
+
+
+
+if sys.argv[1] == "*":
